eef39b85eae7398204044b8a8376c686ca298333
hechuangxin <18236344529@163.com>
2020-07-13 00:06:52 +0800

    0712

--- xuexi/dictionary.py
+++ xuexi/dictionary.py
@@ -1,10 +1,40 @@
-a={"agv":"creey001","bucket":"T-5","name":"木头人","age":"123"}
-print(a["agv"])       #打印key值为"agv"  获取对应的value
-a["sex"]="nan"        #插入key值为"sex"  value值为"nan"
-a["sex"]="nv"          #修改key值为"sex"  value值为"nv"
-print(a)
-a["jobtype"]=["youxian",1,"99"]     #插入key值为"jobtype"  value值为["youxian",1,"99"]
-print(a)
-# print(a.pop("bucket"))     #提取
-a.pop("name")      #把 “ name” 提取之后打印
-print(a)
+from decimal import Decimal
+# a={"agv":"creey001","bucket":"T-5","name":"木头人","age":"123"}
+
+# # print(a["agv"])       #打印key值为"agv"  获取对应的value
+# a["sex"]="nan"        #插入key值为"sex"  value值为"nan"
+# a["sex"]="nv"          #修改key值为"sex"  value值为"nv"
+# print(a)
+# a["jobtype"]=["youxian",1,"99"]     #插入key值为"jobtype"  value值为["youxian",1,"99"]
+# print(a)
+# # print(a.pop("bucket"))     #提取
+# a.pop("name")      #把 “ name” 提取之后打印    删除指定值
+# print(a)
+# a.popitem()     #随机删除一个key值
+# print(a)
+# a.clear()
+# print(a)  #删除列表里所有的值
+# print(a.items())
+# print(a.items())      #key值和value值组合到一起
+# print(a.keys())       #获取所有的key值
+# print(a.values())       #获取所有的value值
+
+# b={"我是","最帅的","ren","我是"}
+# print(b)
+# b=["我是","最帅的","ren","我是"]
+# print(b)
+# b=set(["我是","最帅的","ren","我是"])
+# print(b)
+print(8//5)    #整除
+print(8%5)     #取余
+print(2**3)     #2的3次方
+print(100.1-0.2)
+print(Decimal('1.235')-Decimal('0.1'))    #精确到很高的位置
+print(type(Decimal('1.235') - Decimal('0.1')))     #判断类型  <class 'decimal.Decimal'>
+print(Decimal('56.6') - Decimal('10') == Decimal('46.6'))
+print(8//5==1)               #打印 8除5整除结果是否为1
+
+
+
+
+
